e77d639b927e6d8548f911922ba8c8d132e1e806
zshandilya <zubin_shandilya@yahoo.com>
2016-07-13 01:24:13 +0530

    Assignment 3 for Java Session 1

---
+++ Assignment3.java
@@ -0,0 +1,20 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+
+		int x;
+		char a;
+		
+/*		x='zubin';  int variable can't hold STRING values
+		x=5.25;	    int variable can't hold DECIMAL values	*/
+		
+		x='f';		//int variable can take a CHAR value, but converts it into its corresponding ASCII value
+		a='f';
+		
+		System.out.println("Value of x: " +x+ " (ASCII value of 'f')");
+		System.out.println("Value of a: " +a);
+		
+	}
+
+}
